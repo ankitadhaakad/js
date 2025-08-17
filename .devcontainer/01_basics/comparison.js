6ec87d590f50104f67a8ccfd288c56c2cbfab50b
Ankita Dhakad <ankudhakad444@gmail.com>
2025-08-17 09:15:02 +0000

    comparison donne

---
+++ .devcontainer/01_basics/comparison.js
@@ -0,0 +1,12 @@
+console.log(2>1);
+console.log(2>=1);
+
+console.log(null  >0)//false
+console.log(null==0);//false
+console.log(null >=0);//true  
+//not use these type of code ,code readablity and clean code most important
+
+//=== compare &check datatype
+console.log("2"===2);//false
+
+
--- .devcontainer/01_basics/conversion.js
+++ .devcontainer/01_basics/conversion.js
@@ -10,3 +10,29 @@ let valueLoggedIn = Boolean(isLoggedIn)
 console.log(typeof valueLoggedIn)//boolean
 console.log(valueLoggedIn)//true
 
+
+
+
+//******************operations ***********//
+let value = 3
+let negvalue =-value
+//console.log(negvalue)//-3
+let str1 ="helllo"
+str2 = " ankita"
+str3 = str1 +str2
+console.log(str3);
+
+console.log("1"+2+2);
+console.log(2+2+"1");//esse code nhi likhe() ka use krre 
+console.log(+true)//1
+console.log(+"");//0
+
+let gameCounter = 100
+gameCounter++;//101
+++gameCounter;//101
+console.log(gameCounter);//101
+
+
+
+
+
