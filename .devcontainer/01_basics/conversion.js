a42487be899f64010234ced5d3123a4ab0b3b325
Ankita Dhakad <ankudhakad444@gmail.com>
2025-08-17 08:17:20 +0000

    conversion

---
+++ .devcontainer/01_basics/conversion.js
@@ -0,0 +1,12 @@
+let score = "33abc"  //null=>0  undefined =>NaN  true/false=>1/0
+console.log(typeof score)
+
+let valueInNumber =Number(score)
+console.log(typeof valueInNumber)
+console.log(valueInNumber);
+
+let isLoggedIn =1  //"" to false aaega
+let valueLoggedIn = Boolean(isLoggedIn)
+console.log(typeof valueLoggedIn)//boolean
+console.log(valueLoggedIn)//true
+
